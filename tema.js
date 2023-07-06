62e21b0cccde86f0b5bbf301b77f163d7f4d74b3
ergito <134168866+ergitoesp@users.noreply.github.com>
2023-07-06 13:21:05 +0200

    Add files via upload

---
+++ tema.js
@@ -0,0 +1,27 @@
+let temacambio=document.querySelector(".temacambio");
+function changetheme(a){
+    if(a==1){
+        temacambio.style.background="black"
+    } else if(a==2){
+        temacambio.style.background="blue"
+    } else if(a==3){
+        temacambio.style.background="red"
+    } else if(a==4){
+        temacambio.style.background="purple"
+    } else if(a==5){
+        temacambio.style.background="gray"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
